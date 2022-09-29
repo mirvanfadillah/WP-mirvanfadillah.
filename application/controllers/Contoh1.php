f2c70a86cde4a8eb1160307caeeb1f614301f4c2
MIrvanFadillah1 <fadillahirvan008@icloud.com>
2022-09-30 01:01:11 +0700

    commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php class Contoh1 extends CI_Controller
+ { 
+	public function index()
+	 { 
+		echo "<h1>M IRVAN FADILLAH</h1>"; 
+		echo"Nama saya M IRVAN FADILLAH 
+		Dari kelas 12.3C.30 hobi yang saya sukai motorcross"; 
+	 } 
+}
