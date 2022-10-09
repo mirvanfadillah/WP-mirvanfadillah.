57a033fb41ac95b5a9d6c05c033978ef36bf37a1
MIrvanFadillah1 <fadillahirvan008@icloud.com>
2022-10-09 15:16:05 +0700

    commit

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -5,5 +5,18 @@
 		echo "<h1>M IRVAN FADILLAH</h1>"; 
 		echo"Nama saya M IRVAN FADILLAH 
 		Dari kelas 12.3C.30 hobi yang saya sukai motorcross"; 
-	 } 
+		//$this->load->view('view-latihan1');
+		
+	} public function penjumlahan($n1, $n2) 
+	
+	{ 
+		$this->load->model('Model_latihan1'); 
+		
+		$data['nilai1'] = $n1;
+		$data['nilai2'] = $n2;
+		$data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2); 
+		  
+		  $this->load->view('view-latihan1', $data);  
+ 
+	} 
 }
